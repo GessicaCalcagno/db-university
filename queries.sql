af96cded126991357061f64c49ea1ad655a95799
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-05-29 15:34:29 +0200

    Add: 6, 7, 8, 9, 10

--- queries.sql
+++ queries.sql
@@ -8,8 +8,43 @@ SELECT *
 FROM `courses`
 WHERE `cfu` > 10;
 
+-- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+
+
+
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT * 
 FROM `courses` 
 WHERE `period` = "I semestre"
 AND `year` = 1;
+
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+
+
+
+
+-- 6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * 
+FROM `degrees` 
+WHERE `level` = "magistrale";
+
+-- 7. Da quanti dipartimenti è composta l'università? (12)
+SELECT COUNT(`id`) AS `num_dipartiments`
+FROM `departments`;
+
+
+-- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT * 
+FROM `teachers` 
+WHERE `phone` IS NULL;
+
+-- 9. Inserire nella tabella degli studenti un nuovo record con i propri dati (per il campo degree_id, inserire un valore casuale)
+INSERT INTO `students` ( `degree_id`, `name`, `surname`, `date_of_birth`, `fiscal_code`, `enrolment_date`, `registration_number`, `email`)
+VALUES ("67","Gessica","Calchi", "2000-08-31", "Q10VYW51Z02T871B", "2019-02-21", "620058", "disperazioneoliosutela@gmail.com");
+
+-- 10. Cambiare il numero dell’ufficio del professor Artemide Rizzi in 126
+UPDATE `teachers`
+SET `office_number` = 126
+WHERE `id` = 1;
+
+-- 11. Eliminare dalla tabella studenti il record creato precedentemente al punto 9
