758a1797155f411bb02b0c0a5b918d28aba77569
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-05-29 14:47:37 +0200

    Add: 2 and 4

--- queries.sql
+++ queries.sql
@@ -3,3 +3,13 @@ SELECT *
 FROM `students`
 WHERE YEAR (`date_of_birth`) = 1990;
 
+-- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+SELECT * 
+FROM `courses`
+WHERE `cfu` > 10;
+
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT * 
+FROM `courses` 
+WHERE `period` = "I semestre"
+AND `year` = 1;
